9208717c51638eba82abcfb4ffb26f0bd4780fff
bahatilugusi <bahatilugusi2004@gmail.com>
2024-12-17 16:00:40 +0300

    giving simBanking function functionality and and creating its option in the Main Option

--- banking/banking.cpp
+++ banking/banking.cpp
@@ -7,16 +7,17 @@ void showMenu()
     cout << "1. Check balance\n";
     cout << "2. Deposite \n";
     cout << "3. Withdraw \n";
-    cout << "4. Exit.\n";
+    cout << "4. Simbanking\n";
+    cout << "5. Exit.\n";
     cout << "**********MENU**********" << endl;
 }
 void simBanking()
 {
     cout << "\n\t---SIMBANKING-----\n";
-    cout << "\n Eirtel money.";
+    cout << "Eirtel money.";
     cout << "\nHallo Pesa";
     cout << "\nTiGo Pesa";
-    cout << "\nCAzam Pesa";
+    cout << "\nCAzam Pesa\n\n";
 }
 
 int main()
@@ -52,6 +53,11 @@ int main()
             else
                 cout << "no enough money\n";
             break;
+        case 4:
+            simBanking();
+            break;
+        default:
+            cout << "Invalid choice! Try again.";
         }
-    } while (option != 4);
+    } while (option != 5);
 }
